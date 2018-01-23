7dfe6f34b18c6f693ecae97979e7e5f8916800a6
Nikolay Bolgurtcev <nikbol@transas.com>
2018-01-23 20:24:50 +0300

    added better bot stopping

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -1,24 +1,40 @@
 import logging
+from time import sleep
+from functools import partial
 
 import telegram
 from telegram.ext import Updater, CommandHandler
 from db_interface import get_user, add_user
 from notifier import base_notify
-from functools import partial
 
 logger = logging.getLogger(__name__)
 
 
-def on_start(bot: telegram.Bot, update: telegram.Update):
+class Stopper:
+    def __init__(self):
+        self.stop = False
+
+
+def on_start(bot: telegram.Bot, update: telegram.Update, stopper):
     bot.send_message(update.message.chat_id, 'We get the job done!')
     add_user(update.message.from_user.username, update.message.from_user.id)
+    stopper.stop = True
 
 
-def wait_for_registration(token):
+def ask_for_registration(token):
     updater = Updater(token)
-    updater.dispatcher.add_handler(CommandHandler('start', on_start))
+    stopper = Stopper()
+    updater.dispatcher.add_handler(CommandHandler('start', partial(on_start, stopper=stopper)))
     updater.start_polling()
-    return updater.bot.username
+    link = f"https:/t.me/{updater.bot.username}"
+    print(f'Start conversation to receive notifications: {link}')
+
+    # sleep for 25 seconds or until user registered
+    sleeps_count = 0
+    while not stopper.stop and sleeps_count < 25:
+        sleep(1)
+        sleeps_count += 1
+    updater.stop()
 
 
 def send_telegram_notification(token, username, message, elapsed=0):
@@ -26,18 +42,15 @@ def send_telegram_notification(token, username, message, elapsed=0):
         username = username[1:]
     user = get_user(username)
     if user:
-        Updater(token).bot.send_message(user.telegram_id, message)
-        Updater(token).bot.send_message(user.telegram_id, f"Time: {elapsed}")
+        Updater(token).bot.send_message(user.telegram_id, f"{message}\n\nTime: {elapsed}")
     else:
         logger.warning('No such user: %s', username)
 
 
-def check_for_registration(token, username, message):
+def check_for_registration(token, username, _):
     user = get_user(username)
     if not user:
-        bot_name = wait_for_registration(token)
-        link = f"https:/t.me/{bot_name}"
-        print(f'Start conversation to receive notifications: {link}')
+        ask_for_registration(token)
 
 
 telegram_notify = partial(base_notify, send_telegram_notification, pre_fn=check_for_registration)
